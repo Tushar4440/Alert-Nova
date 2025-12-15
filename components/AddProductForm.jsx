758226486858a2bd3403e34a9db34ed538c72d16
Tushar4440 <tushartewari33@gmail.com>
2025-12-15 15:26:47 +0530

    Some input and buttton implementation and some state changes...

--- components/AddProductForm.jsx
+++ components/AddProductForm.jsx
@@ -1,8 +1,52 @@
-import React from 'react'
+"use client";
 
-const AddProductForm = () => {
+import React, { useState } from 'react'
+import { Input } from './ui/input';
+import { Button } from './ui/button';
+import { Loader2, LoaderIcon } from 'lucide-react';
+
+const AddProductForm = ({ user }) => {
+
+  const [url, setUrl] = useState("")
+  const [loading, setLoading] = useState(false)
+  const handleSubmit = async (e) => {
+
+  }
   return (
-    <div>AddProductForm</div>
+    <>
+    {/* input form */}
+      <form onSubmit={handleSubmit} className='w-full max-w-2xl mx-auto'>
+        <div className='flex flex-col sm:flex-row gap-2'>
+          {/* Link Pasting input */}
+          <Input
+            type="url"
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+            placeholder="Paste the product/item URL (Amazon,Walmart,etc.)"
+            className="h-12 text-base"
+            required
+            disabled={loading}
+          />
+          {/* Button */}
+          <Button className="bg-pink-500 hover:bg-pink-600 h-10 sm:h-12 px-8 cursor-pointer"
+            type="submit"
+            disabled={loading}
+            size={"lg"}
+          >
+            {loading ? (
+              <>
+                <LoaderIcon className='mr-2 h-4 w-4 animate-spin bg-amber-200' />
+                Finding...
+              </>
+            ) : (
+              "Hunt prices"
+            )}
+          </Button>
+        </div>
+      </form>
+
+      {/* Auth model */}
+    </>
   )
 }
 
